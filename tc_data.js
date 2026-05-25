d708d80d95a3134becc00cc6e073f657b4c2fa60
macaosso <tungtung0191@gmail.com>
2026-05-26 05:17:04 +0800

    Add files via upload

---
+++ tc_data.js
@@ -0,0 +1,21 @@
+const forecastInfo = {
+  updated: "05-26 04:00 MST"
+};
+
+const pastPoints = [
+  {lat:6, lon:145, time:"06-08 00H", wind: 40},
+  {lat:7, lon:142, time:"06-09 00H", wind: 45},
+  {lat:8, lon:140, time:"06-10 00H", wind: 55},
+  {lat:9, lon:139, time:"06-11 00H", wind: 65},
+  {lat:10, lon:137, time:"06-12 00H", wind: 85}
+];
+
+const forecastPoints = [
+  {lat:10, lon:137, time:"06-12 00H", wind: 85},
+  {lat:11, lon:133, time:"06-12 12H", wind: 110},
+  {lat:13, lon:130, time:"06-13 00H", wind: 130},
+  {lat:15, lon:123, time:"06-14 00H", wind: 160},
+  {lat:17, lon:120, time:"06-15 00H", wind: 190},
+  {lat:19, lon:117, time:"06-16 00H", wind: 175},
+  {lat:20, lon:113, time:"06-17 00H", wind: 140}
+];
